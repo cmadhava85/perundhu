5168ca3dd5bff96d73e22489ab5407b4501c1361
cmadhava85 <cmadhava85@github.com>
2026-01-08 17:32:08 -0500

    Fix preprod terraform: import firewalls and custom role, fix Cloud Run outputs

--- infrastructure/terraform/modules/cloud_run/outputs.tf
+++ infrastructure/terraform/modules/cloud_run/outputs.tf
@@ -5,7 +5,7 @@ output "service_name" {
 
 output "service_url" {
   description = "URL of the Cloud Run service"
-  value       = google_cloud_run_service.backend.status[0].url
+  value       = try(google_cloud_run_service.backend.status[0].url, "")
 }
 
 output "service_location" {
